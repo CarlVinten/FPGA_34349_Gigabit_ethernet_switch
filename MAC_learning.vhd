27f4d255895578e67eebb840015f0b09369df44c
Carl <carl.vinten@gmail.com>
2026-04-15 15:33:16 +0200

    Mac Learning with testbench implemented

--- MAC_learning.vhd
+++ MAC_learning.vhd
@@ -12,11 +12,12 @@ port (
 	rst : in std_logic;
 	clk : in std_logic;
 	mac_dst : in mac_input;
-	mac_dst_ready : out std_logic;
-	mac_dst_valid : in std_logic_vector(NUM_PORTS - 1 downto 0);
 	mac_src : in mac_input;
-	mac_src_ready : out std_logic_vector(NUM_PORTS - 1 downto 0);
-	mac_src_valid : in std_logic_vector(NUM_PORTS - 1 downto 0)
+	valid : in std_logic_vector(NUM_PORTS - 1 downto 0);
+	ready: out std_logic_vector(NUM_PORTS - 1 downto 0);
+	port_output : out mac_output;
+	output_valid : out std_logic_vector(NUM_PORTS - 1 downto 0);
+	output_ready : in std_logic_vector(NUM_PORTS - 1 downto 0)
 	);
 end MAC_learning;
 
@@ -31,45 +32,132 @@ ARCHITECTURE struc OF MAC_learning IS
 		);
 	end component;
 	SIGNAL address: std_logic_vector(12 downto 0);
-	SIGNAL dst_rr: integer range 0 to 1 := 0;
+	SIGNAL m_data: std_logic_vector(63 downto 0);
+	SIGNAL m_wren: std_logic := '0';
+	SIGNAL m_out: std_logic_vector(63 downto 0);
+	SIGNAL rr: integer range 0 to 3 := 0;
+	SIGNAL process_mac: std_logic := '0';
+	SIGNAL port_to_check: integer range 0 to 3 := 0;
+	SIGNAL port_one_hot: std_logic_vector(3 downto 0) := "0000";
+	SIGNAL has_data: std_logic_vector(NUM_PORTS - 1 downto 0) := "0000";
+	SIGNAL d_mac: mac_input;
+	SIGNAL s_mac: mac_input; 
+	SIGNAL mac_check_state : integer range 0 to 2 := 0;
+	SIGNAL mac_check : std_logic_vector(63 downto 0) := x"0000000000000000";
 
-	SIGNAL loading_dst: std_logic := '0';
-	SIGNAL dst_port: integer range 0 to 1 := 00;
-	SIGNAL dst_mac_buf: std_logic_vector(6 downto 0);
-	SIGNAL dst_mac_buf_empty: std_logic := '1';
-	SIGNAL dst_ready: std_logic := '0';
-	SIGNAL loading_src: std_logic := '0';
-	SIGNAL src_port: std_logic_vector(1 downto 0) := "00";
 BEGIN
 
-	mac_dst_ready <= dst_ready;
-	process(clk)
-	begin
-	if rising_edge(clk) then -- check en port en ad gangen
-		dst_rr <= dst_rr + 1;
-		if(dst_ready = '1') then
-			if(mac_dst_valid(dst_rr) = '1') then
-				loading_dst <= '1';
-				dst_port <= dst_rr;
-				mac_dst_ready <= '0';
-			end if;
-		end if;
-	end if;
-	end process;
--- process to load mac adresses
-	process(clk)
-	begin
-		if rising_edge(clk) then
-			if(loading_dst = '1') then
-				dst_mac_buf <= mac_dst(dst_port);
-				loading_dst <= '0';
-				dst_mac_buf_empty <= '0';
-			end if;
-		end if;
-	end process;
--- process for src mac address
+	mem : mac_learning_mem
+		port map(
+			address => address,
+			clock => clk,
+			data => m_data,
+			wren => m_wren,
+			q => m_out
+		);
 
--- process for dest mac address 
+	process(clk)
+		begin
+		if(rising_edge(clk)) then
+		ready <= "0000";
+			if has_data(0) = '0' then
+				ready(0) <= '1';
+			end if;
+			if has_data(1) = '0' then
+				ready(1) <= '1';
+			end if;
+			if has_data(2) = '0' then
+				ready(2) <= '1';
+			end if;
+			if has_data(3) = '0' then
+				ready(3) <= '1';
+			end if;
+		end if;
+
+		if (rising_edge(clk)) then
+			if (valid(0) = '1') and (has_data(0) = '0') then
+				has_data(0) <= '1';
+				d_mac(0) <= mac_dst(0);
+				s_mac(0) <= mac_src(0);
+			end if;
+			if (valid(1) = '1') and (has_data(1) = '0') then
+				has_data(1) <= '1';
+				d_mac(1) <= mac_dst(1);
+				s_mac(1) <= mac_src(1);
+			end if;
+			if (valid(2) = '1') and (has_data(2) = '0') then
+				has_data(2) <= '1';
+				d_mac(2) <= mac_dst(2);
+				s_mac(2) <= mac_src(2);
+			end if;
+			if (valid(3) = '1') and (has_data(3) = '0') then
+				has_data(3) <= '1';
+				d_mac(3) <= mac_dst(3);
+				s_mac(3) <= mac_src(3);
+			end if;
+
+
+		end if;
+
+		if (rising_edge(clk)) then
+			if(process_mac = '0') then
+				rr <= ((rr + 1) mod 4);
+				if(has_data(rr) = '1') then
+					process_mac <= '1';
+					port_to_check <= rr;
+				elsif(has_data(rr + 1) = '1') then
+					process_mac <= '1';
+					port_to_check <= rr + 1;
+				elsif(has_data(rr + 2) = '1') then
+					process_mac <= '1';
+					port_to_check <= rr + 2;
+				elsif(has_data(rr + 3) = '1') then
+					process_mac <= '1';
+					port_to_check <= rr + 3;
+				end if;
+			end if;
+		end if;
+
+		if rising_edge(clk) then
+			m_wren <= '0';
+			output_valid <= "0000";
+			case port_to_check is
+				when 0 => port_one_hot <= "0001";
+				when 1 => port_one_hot <= "0010";
+				when 2 => port_one_hot <= "0100";
+				when 3 => port_one_hot <= "1000";
+			end case;
+			if(process_mac = '1') then
+				case mac_check_state is
+					when 0 =>
+						address <= d_mac(port_to_check)(12 downto 0);
+						mac_check_state <= 1;
+					when 1 =>
+						if(m_out(47 downto 0) = d_mac(port_to_check))then
+							port_output(port_to_check) <= m_out(51 downto 48);
+						else
+							port_output(port_to_check) <= not(port_one_hot);
+						end if;
+						output_valid(port_to_check) <= '1';
+						if(output_ready(port_to_check) = '1') then
+							mac_check_state <= 2;
+						end if;
+					when 2 =>
+						output_valid(port_to_check) <= '0';
+						m_wren <= '1';
+						address <= s_mac(port_to_check)(12 downto 0);
+						m_data <= x"000" & port_one_hot & s_mac(port_to_check);
+						process_mac <= '0';
+						mac_check_state <= 0;
+						has_data(port_to_check) <= '0';
+				end case;
+			end if;
+		end if;
+
+
+
+	end process;	
+
 
 
 END struc;
--- MAC_learning_tb.vhd
+++ MAC_learning_tb.vhd
@@ -8,76 +8,66 @@ use work.global_var.all;
 
 entity test_mac_learning is
     PORT ( 
+	
 	port_output      : out mac_output;
 	output_valid     : out std_logic_vector(NUM_PORTS - 1 downto 0)
 );
 end;
 
-architecture only of test_parallel_fcs is
-COMPONENT fcs_check_parallel
-    	PORT ( clk   : in std_logic;
-            reset : in std_logic;
-	        start_of_frame : IN std_logic;
-	        end_of_frame : IN std_logic;
-	        data_in : IN std_logic_vector(7 downto 0);
-	        fcs_error : OUT std_logic);
-END COMPONENT ;
-
-SIGNAL clk   : std_logic := '0';    
-SIGNAL reset : std_logic := '0';
-SIGNAL pakke1 : std_logic_vector(511 downto 0);
-SIGNAL pakke2 : std_logic_vector(511 downto 0);
-SIGNAL pakke3 : std_logic_vector(511 downto 0);
-SIGNAL index : integer range -100 to 511 := 511;
-SIGNAL start_of_frame : std_logic := '0';
-SIGNAL end_of_frame : std_logic := '0';
-SIGNAL data_in : std_logic_vector(7 downto 0) := x"00";
---SIGNAL fcs_error : bit := '0';
-
+architecture only of test_mac_learning is
+	component MAC_learning
+		port(
+			rst : in std_logic;
+			clk : in std_logic;
+			mac_dst : in mac_input;
+			mac_src : in mac_input;
+			valid: in std_logic_vector(NUM_PORTS - 1 downto 0);
+			ready: out std_logic_vector(NUM_PORTS - 1 downto 0);
+			port_output : out mac_output;
+			output_valid : out std_logic_vector(NUM_PORTS - 1 downto 0);
+			output_ready : in std_logic_vector(NUM_PORTS - 1 downto 0)
+		);
+	end component;
+	SIGNAL clk : std_logic := '0';
+	SIGNAL rst : std_logic := '0';
+	SIGNAL tb_valid : std_logic_vector(3 downto 0) := "1111";
+	SIGNAL tb_dmac : mac_input;
+	SIGNAL tb_smac : mac_input;
+	SIGNAL tb_ready : std_logic_vector(NUM_PORTS - 1 downto 0) := "0000";
+	SIGNAL tb_output_ready	: std_logic_vector(NUM_PORTS - 1 downto 0) := x"F";
 begin
+	mac : MAC_learning
+		port map(
+			rst => rst,
+			clk => clk,
+			mac_dst => tb_dmac,
+			mac_src => tb_smac,
+			valid => tb_valid,
+			ready => tb_ready,
+			port_output => port_output,
+			output_valid => output_valid,
+			output_ready => tb_output_ready
+		);
 
-pakke1 <= x"00_10_A4_7B_EA_80_00_12_34_56_78_90_08_00_45_00_00_2E_B3_FE_00_00_80_11_05_40_C0_A8_00_2C_C0_A8_00_04_04_00_04_00_00_1A_2D_E8_00_01_02_03_04_05_06_07_08_09_0A_0B_0C_0D_0E_0F_10_11_E6_C5_3D_B2";
-pakke2 <= x"FF_EF_5B_84_EA_80_00_12_34_56_78_90_08_00_45_00_00_2E_B3_FE_00_00_80_11_05_40_C0_A8_00_2C_C0_A8_00_04_04_00_04_00_00_1A_2D_E8_00_01_02_03_04_05_06_07_08_09_0A_0B_0C_0D_0E_0F_10_11_19_3A_C2_4D";
-pakke3 <= x"00_10_A4_7B_EA_80_00_12_34_56_78_90_08_00_45_00_00_2E_B3_FE_00_00_80_11_05_40_C0_A8_00_2C_C0_A8_00_04_04_00_04_00_00_1A_2D_E8_00_01_02_03_04_05_06_07_08_09_0A_0B_0C_0D_0E_0F_10_11_19_3A_C2_4D";
-dut : fcs_check_parallel 
-   PORT MAP (
-   clk => clk,
-   reset => reset,
-   data_in => data_in,
-   start_of_frame => start_of_frame,
-   end_of_frame => end_of_frame,
-   fcs_error => fcs_error);
+	tb_dmac(0) <= x"FFFFFFEEEEEE";
+	tb_dmac(1) <= x"DDDDDDCCCCCC";
+	tb_dmac(2) <= x"BBBBBBAAAAAA";
+	tb_dmac(3) <= x"999999888888";
 
+	tb_smac(0) <= x"777777666666";
+	tb_smac(1) <= x"555555444444";
+	tb_smac(2) <= x"333333222222";
+	tb_smac(3) <= x"111111000000";
 clock : PROCESS
    begin
    wait for 3 ns; clk  <= not clk;
 end PROCESS clock;
 
 stimulus : PROCESS(clk)
-   begin
-      if rising_edge(clk) then
-         if index >= 0 then
-         data_in <= pakke1((index) downto (index - 7));
-         end if;
-	      index <= index - 8;
+   	begin
+      	if rising_edge(clk) then
 
-         if index = 511 then
-            start_of_frame <= '1';
-            end_of_frame <= '0'; 
-         else
-            start_of_frame <= '0';
-         end if;
-		end_of_frame <= '0';
-         if index = 31 then
-            end_of_frame <= '1';
-         end if;
-
-	      if reset = '1' then
-            index <= 511;
-         end if;
-      end if;
-   
-      
+      	end if;  
 end PROCESS stimulus;
 end only;
 
