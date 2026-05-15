5c5ce943fae14ebd69165f1556d4c24bea556568
Janus Meier <meier.janus@gmail.com>
2026-05-15 13:46:43 +0200

    Update ethernet_switch_4port_tb.vhd

--- ethernet_switch_4port_tb.vhd
+++ ethernet_switch_4port_tb.vhd
@@ -37,28 +37,19 @@ architecture tb of ethernet_switch_4port_tb is
 
     -- Clock period
     constant CLK_PERIOD : time := 10 ns;
-    procedure read_packet_file(
-        filename    : string;
-        port_data   : out std_logic_vector(7 downto 0);
-        port_valid  : out boolean;
-        file_handle : inout text
-    ) is
-        variable line_buffer : line;
-        variable data_value  : std_logic_vector(7 downto 0);
-        variable read_ok     : boolean;
-    begin
-        port_valid := false;
-        port_data := (others => '0');
-        
-        if not endfile(file_handle) then
-            readline(file_handle, line_buffer);
-            hread(line_buffer, data_value, read_ok);
-            if read_ok then
-                port_data := data_value;
-                port_valid := true;
-            end if;
-        end if;
-    end procedure read_packet_file;
+    
+    -- Ethernet packet data
+    type byte_array_t is array(integer range <>) of std_logic_vector(7 downto 0);
+    constant ETHERNET_FRAME : byte_array_t(0 to 63) := (
+        x"00", x"10", x"A4", x"7B", x"EA", x"80", x"00", x"12",
+        x"34", x"56", x"78", x"90", x"08", x"00", x"45", x"00",
+        x"00", x"2E", x"B3", x"FE", x"00", x"00", x"80", x"11",
+        x"05", x"40", x"C0", x"A8", x"00", x"2C", x"C0", x"A8",
+        x"00", x"04", x"04", x"00", x"04", x"00", x"00", x"1A",
+        x"2D", x"E8", x"00", x"01", x"02", x"03", x"04", x"05",
+        x"06", x"07", x"08", x"09", x"0A", x"0B", x"0C", x"0D",
+        x"0E", x"0F", x"10", x"11", x"E6", x"C5", x"3D", x"B2"
+    );
 
 begin
     -- DUT instantiation
@@ -90,11 +81,7 @@ begin
     
     -- Test stimulus
     stimulus : process
-        file rx0_file : text;
-        
-        variable rx0_valid : boolean := false;
-        variable rx0_data : std_logic_vector(7 downto 0);
-        
+        variable packet_index : integer;
     begin
         -- Reset assertion
         rst <= '1';
@@ -107,24 +94,13 @@ begin
         rst <= '0';
         wait for 50 ns;
         
-        -- Open packet file
-        file_open(rx0_file, "fpga_test_packets/packet_01.txt", read_mode);
-        
         -- Set RX_control high for entire packet reception
         RX_control(0) <= '1';
         RX_control(3 downto 1) <= "000";
         
-        -- Read and apply packets until file is exhausted
-        loop
-            -- Read from packet file
-            read_packet_file("fpga_test_packets/packet_01.txt", rx0_data, rx0_valid, rx0_file);
-            
-            if not rx0_valid then
-                exit;  -- Exit loop when file exhausted
-            end if;
-            
-            -- Apply data to port 0
-            RX(0) <= rx0_data;
+        -- Apply ethernet frame data to port 0
+        for packet_index in ETHERNET_FRAME'range loop
+            RX(0) <= ETHERNET_FRAME(packet_index);
             
             -- Keep other ports inactive
             RX(1) <= (others => '0');
@@ -132,9 +108,6 @@ begin
             RX(3) <= (others => '0');
             wait for CLK_PERIOD;
         end loop;
-        
-        -- Close file
-        file_close(rx0_file);
         
         -- Stop all signals
         RX_control <= "0000";
